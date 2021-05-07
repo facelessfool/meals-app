34766aaec5e6ca529a35dbc0b00fc8e4604e82b8
Kajal Chingsubam <kchings1@binghamton.edu>
2021-05-07 10:27:24 -0400

    added scrollable grid items with Flatlist

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,8 +1,34 @@
 import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Button, FlatList } from "react-native";
+import { CATEGORIES } from "../data/dummy-data";
 
-const CategoriesScreen = (props) => {};
+const renderGridItem = (itemData) => {
+  return (
+    <View style={styles.gridItem}>
+      <Text>{itemData.item.title}</Text>
+    </View>
+  );
+};
 
-const styles = StyleSheet.create({});
+const CategoriesScreen = (props) => {
+  console.log("props", props);
+
+  return (
+    <FlatList data={CATEGORIES} renderItem={renderGridItem} numColumns={2} />
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  gridItem: {
+    flex: 1,
+    margin: 20,
+    height: 150,
+  },
+});
 
 export default CategoriesScreen;
